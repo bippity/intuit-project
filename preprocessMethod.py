7f5e7ab87d7076317d3e84752108674ded46324d
bippity <bippity@users.noreply.github.com>
2020-04-02 19:36:42 -0500

    Added Thresholding method

--- preprocessMethod.py
+++ preprocessMethod.py
@@ -2,8 +2,10 @@
 # Runs a preprocessing method against the intermediate dataset and outputs them
 # into the "preprocessed" folder
 
-# Denoising
-# https://docs.opencv.org/3.4/d5/d69/tutorial_py_non_local_means.html
+# Binarization - Adaptive Thresholding
+# Adaptive thresholding is used instead of Simple thresholding because the image may have different lighting conditions
+# in different areas.
+# https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
 
 import cv2 as cv
 import numpy as np
@@ -13,7 +15,7 @@ from matplotlib import pyplot as plt
 
 
 def runPreprocess(image_dir):
-    imageCount = 100
+    imageCount = 1
     images = []
     tempImages = [] 
     times = []
@@ -26,11 +28,15 @@ def runPreprocess(image_dir):
     # Preprocess the images and store them in a temp list
     for i in range(len(images)):
         startTime = int(round(time.time() * 1000))
-        # Open the image file
-        tempImage = cv.imread(images[i])
+        # Open the image file in greyscale (0)
+        tempImage = cv.imread(images[i], 0)
+        # Blurring (averaging)/Smoothing the image to reduce noise
+        #Maybe not use this??
+        tempImage = cv.medianBlur(tempImage, 5)
         
         # Do the preprocessing stuff here
-        tempImage = cv.fastNlMeansDenoisingColored(tempImage, None, 10, 10, 7, 21)
+        # Apply Adaptive thresholding method - Guassian threshold
+        tempImage = cv.adaptiveThreshold(tempImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
         
         # The preprocesed images are saved temporarily in memory instead of written into output directory
         # so calculating the actual processing time won't be affected
@@ -53,7 +59,7 @@ def main():
     
     
     # Output processed images into output directory
-    output_dir = "results"
+    output_dir = "denoised"
     try:
         os.makedirs(output_dir)
     except FileExistsError:
@@ -63,6 +69,6 @@ def main():
         tempImage = processedImages[i]
         cv.imwrite(output_dir + "/W2_XL_input_noisy_" + str(1000 + i) + ".jpg", tempImage)
         
-    print("Saved processed images to results directory")
+    print("Saved processed images to denoised directory")
 
 main()
